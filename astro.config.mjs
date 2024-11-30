a41886fe6be28510f6757cdf75d168dbef600a33
DataNamura <datanamura@gmail.com>
2024-11-30 16:18:08 +0000

    Initial commit: Adding project files with shadcn components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,48 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react()
+  ],
+  site: 'https://eduardoconstrucciones.com', 
+  base: '/',
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  prefetch: {
+    prefetchAll: true
+  },
+  vite: {
+    build: {
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vendor': [
+              'react',
+              'react-dom'
+            ]
+          }
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom'],
+      exclude: []
+    },
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    ssr: {
+      noExternal: ['@shadcn/ui']
+    }
+  }
+});
